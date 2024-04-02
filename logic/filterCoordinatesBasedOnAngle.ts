1428af0466bd6696d9da5eb396802e408c3d556c
catosaurusrex2003 <mohdmehdi2003@gmail.com>
2024-04-02 23:58:58 +0530

    angle based filtering

---
+++ logic/filterCoordinatesBasedOnAngle.ts
@@ -0,0 +1,30 @@
+import { coordinateType } from "../types";
+
+const threshold = 10;
+
+export function filterCoordinatesBasedOnAngle(coordinates: coordinateType[]) {
+  const result = [];
+  let prevAngle = null;
+
+  for (let i = 0; i < coordinates.length - 1; i++) {
+    const slope =
+      (coordinates[i + 1].latitude - coordinates[i].latitude) /
+      (coordinates[i + 1].longitude - coordinates[i].longitude);
+    const angle = Math.atan(slope) * (180 / Math.PI);
+    console.log(angle);
+
+    if (
+      prevAngle === null ||
+      Math.abs(angle - prevAngle) > threshold
+    ) {
+      result.push(coordinates[i]);
+    }
+
+    prevAngle = angle;
+  }
+
+  // Add the last coordinate
+  result.push(coordinates[coordinates.length - 1]);
+
+  return result;
+}
--- main.ts
+++ main.ts
@@ -1,16 +1,14 @@
-// import { coordinateType } from "./types";
 import coordinates from "./assets/data.json";
-// import { makeJson } from "./helpers/makejson";
 import { makeDesmosString } from "./helpers/makeDesmosString";
+import { filterCoordinatesBasedOnAngle } from "./logic/filterCoordinatesBasedOnAngle";
 import { filterCoordinatesBasedOnSlope } from "./logic/filterCoordinatesBasedOnSlope";
 
 console.log(`original array length : ${coordinates.length}`);
-const filteredCoordinates = filterCoordinatesBasedOnSlope(coordinates);
+// const filteredCoordinates = filterCoordinatesBasedOnSlope(coordinates);
+const filteredCoordinates = filterCoordinatesBasedOnAngle(coordinates);
 console.log(`filtered arrayy length : ${filteredCoordinates.length}`);
-// makeJson(filterCoordinates, "filtered");
-console.log(filteredCoordinates);
 
 const meow1 = makeDesmosString(coordinates);
-console.log(meow1,"\n\n\n\n\n\n\n");
+console.log(meow1, "\n\n\n\n\n\n\n");
 const meow2 = makeDesmosString(filteredCoordinates);
 console.log(meow2);
